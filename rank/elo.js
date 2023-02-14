6522f7e2ab799fdabca7d881f334d91c99ca00eb
Derek Anderson <dmikey@users.noreply.github.com>
2023-02-14 04:23:32 +0000

    comments

--- rank/elo.js
+++ rank/elo.js
@@ -1,55 +1,69 @@
 export function calculateElo(computers, tasks, distribution, results) {
+  // Create an object mapping task IDs to task objects
   const taskIdToTask = tasks.reduce((map, task) => {
     map[task.taskId] = task;
     return map;
   }, {});
 
+  // Create an object mapping computer names to arrays of results for that computer
   const computerToResults = results.reduce((map, result) => {
+    // Find the task associated with this result
     const task = taskIdToTask[result.taskId];
+    // If there is no task associated with this result, return the map as is
     if (!task) {
       return map;
     }
+    // Find the computer that was assigned this task
     const computer = Object.keys(distribution).find((key) => {
       const match = Object.values(distribution[key]).find(
         (distributedTask) => distributedTask.taskId === task.taskId
       );
-
+      // If a match is found, return true
       if (match) {
         return true;
       }
+      // Otherwise, return false
       return false;
     });
-
+    // If there is no computer assigned to this task, return the map as is
     if (!computer) {
       return map;
     }
+    // If this is the first result for this computer, create an array for it
     if (!map[computer]) {
       map[computer] = [];
     }
+    // Add this result to the array for this computer
     map[computer].push(result);
+    // Return the updated map
     return map;
   }, {});
 
+  // For each computer, calculate its updated ELO score
   return computers.map((computer) => {
+    // Find the results for this computer
     const results = computerToResults[computer.name];
+    // If there are no results for this computer
     if (!results || results.length === 0) {
-      // Check to see if results were in the distribution.
+      // Check to see if this computer was assigned any tasks
       const distributed = distribution[computer.name];
       if (distributed) {
+        // If this computer was assigned tasks but there are no results, decrease its ELO score
         let updatedElo = Object.keys(computer.elo).reduce((acc, key) => {
           acc[key] = computer.elo[key] - 0.1;
           acc[key] = isNaN(acc[key]) ? 0 : acc[key];
           return acc;
         }, {});
-
+        // Return the computer with the updated ELo score
         return {
           ...computer,
           elo: updatedElo,
         };
       }
+      // If this computer was not assigned any tasks, return it as is
       return computer;
     }
-
+    // If there are results for this computer, calculate its average execution time
     const averageResult = results.reduce(
       (acc, result) => {
         acc.executionTime += result.executionTime;
@@ -57,7 +71,7 @@ export function calculateElo(computers, tasks, distribution, results) {
       },
       { executionTime: 0 }
     );
-
+    // Update the ELO score for this computer based on its average execution time
     let updatedElo = {
       execution:
         computer.elo.execution + averageResult.executionTime / results.length,
--- rank/nascar.js
+++ rank/nascar.js
@@ -1,55 +1,79 @@
 export function calculateNascar(computers, tasks, distribution, results) {
+  // Create a map of task IDs to tasks
   const taskIdToTask = tasks.reduce((map, task) => {
+    // For each task, add an entry to the map with the task ID as the key and the task object as the value
     map[task.taskId] = task;
+    // Return the updated map
     return map;
   }, {});
 
+  // Create a map of computer names to their results
   const computerToResults = results.reduce((map, result) => {
+    // Get the task associated with the result
     const task = taskIdToTask[result.taskId];
+    // If the task does not exist, return the map as-is
     if (!task) {
       return map;
     }
+    // Get the computer that was assigned the task
     const computer = Object.keys(distribution).find((key) => {
+      // Find the task in the list of tasks assigned to the computer
       const match = Object.values(distribution[key]).find(
         (distributedTask) => distributedTask.taskId === task.taskId
       );
-
+      // If a match is found, return true
       if (match) {
         return true;
       }
+      // Otherwise, return false
       return false;
     });
-
+    // If the computer was not found, return the map as-is
     if (!computer) {
       return map;
     }
+    // If this is the first result for the computer, create an array for the results
     if (!map[computer]) {
       map[computer] = [];
     }
+    // Add the result to the array of results for the computer
     map[computer].push(result);
+    // Return the updated map
     return map;
   }, {});
 
+  // Create an array of objects, each representing a computer and its results
   const computersWithResults = Object.keys(computerToResults).map((key) => ({
     name: key,
     results: computerToResults[key],
   }));
 
+  // Sort the computers based on their average execution time
   computersWithResults.sort((a, b) => {
+    // Calculate the average execution time for each computer
     const aAvg =
       a.results.reduce((acc, result) => acc + result.executionTime, 0) /
       a.results.length;
     const bAvg =
       b.results.reduce((acc, result) => acc + result.executionTime, 0) /
       b.results.length;
+    // Compare the average execution times
     return aAvg - bAvg;
   });
 
+  // Get the total number of computers with results
+  const totalComputers = computersWithResults.length;
+  // Set the top score to 1
+  const topScore = 1;
+
+  // Update the NASCAR ranking for each computer
   return computers.map((computer) => {
+    // Find the computer in the list of computers with results
     const computerWithResults = computersWithResults.find(
       (c) => c.name === computer.name
     );
     let updatedNascar = {};
+    // If the computer does not have any results
     if (!computerWithResults) {
       // Check to see if results were in the distribution.
       const distributed = distribution[computer.name];
@@ -59,20 +83,23 @@ export function calculateNascar(computers, tasks, distribution, results) {
         }
         updatedNascar = {
           ...computer.nascar,
-          finish: (computer.nascar.finish || 0) - 5,
+          rank: computer.nascar.rank
+            ? computer.nascar.rank - topScore / totalComputers
+            : 0,
         };
       } else {
         updatedNascar = computer.nascar || {};
       }
     } else {
       updatedNascar = {
-        finish: computersWithResults.indexOf(computerWithResults) + 1,
+        rank:
+          topScore -
+          (topScore / totalComputers) *
+            computersWithResults.indexOf(computerWithResults),
       };
     }
 
-    updatedNascar.finish = isNaN(updatedNascar.finish)
-      ? 0
-      : updatedNascar.finish;
+    updatedNascar.rank = isNaN(updatedNascar.rank) ? 0 : updatedNascar.rank;
     return {
       ...computer,
       nascar: updatedNascar,
